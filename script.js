f0d4da93ef4d2672ac8bfd97210bd1ee7c8a52a7
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-03-15 18:18:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort((a,b)=>{a-b});
+
+	for(var i=0;i<arr.length;i++)
+	{
+		var first=i+1;
+		var last= arr.length-1;
+
+		while(first<last)
+		{
+			if(arr[i]+arr[first]+arr[last]==target){
+				return target;
+			}
+			else if(arr[i]+arr[first]+arr[last]<target){
+				first++;
+			}
+			else{
+				last--;
+				
+			}
+		}
+	}
   
 }
 
